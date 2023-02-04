673a526fdf3f8ba688302e95c137949545ac67cd
Shilu Tudu <shilutudu123@gmail.com>
2023-02-04 21:42:12 +0530

    added basic postgres connection code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("java")
+//    id("java")
+    application
 }
+
 
 group = "org.iitrpr"
 version = "1.0-SNAPSHOT"
@@ -12,7 +14,15 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("info.picocli:picocli:4.7.1")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.1")
+    runtimeOnly("org.postgresql:postgresql:42.3.1")
+    implementation("io.github.cdimascio:dotenv-java:2.3.2")
 }
+
+//application {
+//
+//}
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
